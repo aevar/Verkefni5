ac1c163138570b9a122c45894d67faa37d2594ff
aevar <aevar86@gmail.com>
2012-03-22 22:26:11 +0000

    First commit

---
+++ Verkefni5 - Music Mashup/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="MusicMashup.MvcApplication" Language="C#" %>
---
+++ Verkefni5 - Music Mashup/Models/MusicModel.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.ComponentModel.DataAnnotations;
+
+namespace MusicMashup.Models
+{
+    public class MusicModel
+    {
+        [Required]
+        string Title { get; set; }
+        [Required]
+        string Artist { get; set; }
+        string Disk { get; set; }
+        int Track { get; set; }
+        int Length { get; set; }
+        string Genere { get; set; }
+    }
+}
